6acb83ed20e842e06efbd927ffad233ec13c7b79
Readytowork co. ltd <readytowork@192.168.1.10>
2023-06-18 10:45:25 +0545

    dynamic  metadata

--- apps/car-showcase/app/details/[id]/page.tsx
+++ apps/car-showcase/app/details/[id]/page.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export const metadata = {
-  title: 'Detail page',
-  description: 'detail page',
-};
+// export const metadata = {
+//   title: 'Detail page',
+//   description: 'detail page',
+// };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  console.log('id', params.id);
+  return {
+    title: `detail of car ${params.id}`,
+    description: 'car detail page',
+  };
+}
 const CarDetail = () => {
   return <div className="py-32 padding-x max-width">car detail page</div>;
 };
